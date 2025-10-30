db3b6335dcf1bdecdeacd677c150ad9547822f69
trkturker <tturk753@gmail.com>
2025-10-30 21:12:26 +0300

    30.10

---
+++ app/_layout.tsx
@@ -0,0 +1,10 @@
+import '../global.css';
+import { Stack } from 'expo-router';
+
+export default function Layout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+}
